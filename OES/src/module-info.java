fc02f4b86ed9173548cf3b1469c609da7b039cb1
Kavan teja. V <tejakavan@gmail.com>
2024-11-19 21:33:05 +0530

    first commit

---
+++ OES/src/module-info.java
@@ -0,0 +1,13 @@
+module OES {
+	exports startTest;
+	exports database;
+	exports admin;
+	exports course;
+	exports main;
+	exports login;
+	exports results;
+	exports user;
+
+	requires java.desktop;
+	requires java.sql;
+}
